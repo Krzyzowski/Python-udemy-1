1d72d2db6dd8d6a4272d52696eb89be1507a0647
Krzyzowski <128824402+Krzyzowski@users.noreply.github.com>
2025-01-25 12:19:32 +0100

    Add files via upload

---
+++ Section3_15_Tuples.py
@@ -0,0 +1,18 @@
+
+numbers = (1,2,3) #tuple
+
+numery = [1,2,3] #list
+
+print(type(numbers))
+
+print(type(numery))
+
+print(numbers[0])
+
+#numbers[0] = 15 # error
+
+numery[0] = 15
+
+print(numery)
+#print(numbers) #
+
